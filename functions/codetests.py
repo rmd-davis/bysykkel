95e5b31ee52e08a19e6e98e115e7b4b56ed875d2
rmd-davis <62782186+rmd-davis@users.noreply.github.com>
2022-04-10 20:35:17 +0200

    update codetests

--- functions/codetests.py
+++ functions/codetests.py
@@ -1,6 +1,6 @@
-class codetests:
-    def test1(input_data):
-        test_data = 1
-        
-        assert input_data == test_data, f"\nYour output did not match the expected output 🙃🥴 \nGot: {input_data} \nExpected: {test_data}"
-        print('Test passed, your output matched the expected output!')
+
+def test1(input_data):
+    test_data = 1
+    
+    assert input_data == test_data, f"\nYour output did not match the expected output 🙃🥴 \nGot: {input_data} \nExpected: {test_data}"
+    print('Test passed, your output matched the expected output!')
