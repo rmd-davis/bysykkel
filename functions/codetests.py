f03f7e3d0eb3bccd6a0baffab9b5f7588a9b4ffe
rmd-davis <62782186+rmd-davis@users.noreply.github.com>
2022-04-10 18:00:17 +0200

    Create codetests.py

---
+++ functions/codetests.py
@@ -0,0 +1,8 @@
+class codetests:
+    def test1(input_data):
+        test_data = 1
+        if input_data == test_data:
+            print('Test Passed')
+
+        raise ValueError("Data test failed")
+        
